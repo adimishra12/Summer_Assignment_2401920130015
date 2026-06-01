964add3b64194699e913a08e0737e4f10bb4dcb8
ADITYA MISHRA <adi.mishra.ayush1234@gmail.com>
2026-06-01 12:54:20 +0530

    Refactor maxProfit function in stock trading solution
    Refactor maxProfit function to simplify variable names and logic.

--- Week_1/Day_1/Best_Time_To_Buy_And_Sell_Stock.cpp
+++ Week_1/Day_1/Best_Time_To_Buy_And_Sell_Stock.cpp
@@ -1,29 +1,17 @@
-#include <iostream>
-#include <vector>
-
 class Solution {
 public:
-    int maxProfit(std::vector<int>& prices) {
-        if (prices.empty())
-            return 0;
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0];
+        int profit=0;
+        for (int i = 1; i < prices.size(); i++) 
+        {
+            int diff = prices[i]- min ;
+            if (prices[i] < min)
+                min = prices[i];
 
-        int minPrice = prices[0];
-        int profit = 0;
-        for (int i = 1; i < static_cast<int>(prices.size()); i++) {
-            int diff = prices[i] - minPrice;
-            if (prices[i] < minPrice)
-                minPrice = prices[i];
             else if (diff > profit)
                 profit = diff;
         }
         return profit;
     }
 };
-
-int main() {
-    Solution solution;
-    std::vector<int> prices = {7, 1, 5, 3, 6, 4};
-    int result = solution.maxProfit(prices);
-    std::cout << "Max Profit: " << result << std::endl;
-    return 0;
-}
